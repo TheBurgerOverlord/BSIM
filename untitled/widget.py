6e3965eab5a081a5b053af48ee4c13093e4e65cd
The Burger Overlord <130556657+TheBurgerOverlord@users.noreply.github.com>
2026-07-19 11:56:32 +0200

    Return point to classes

---
+++ untitled/widget.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import sys
+from PyQt6.QtCore import QDir
+from PySide6.QtWidgets import QApplication, QWidget, QFileSystemModel
+from PyQt6 import uic
+
+#defaultPath = QDir.homePath()
+defaultPath = "/run/media/system/Data/BSIM/testingStorage"
+currentPath = defaultPath
+
+
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = uic.loadUi("../interface.ui")
+    window.show()
+    sys.exit(app.exec())
